913c708b654d55b0fa1c9040e7cb819ce270a97c
Rajkamalprocoder2000 <SINGHRajkamal90@gmail.com>
2026-02-17 00:13:39 +0530

    fix: broaden supabase pooler region fallback list

--- includes/db.php
+++ includes/db.php
@@ -56,22 +56,44 @@ function db(): PDO
             'us-east-1',
             'us-west-1',
             'us-west-2',
+            'ca-central-1',
             'eu-west-1',
+            'eu-west-2',
+            'eu-west-3',
             'eu-central-1',
+            'eu-central-2',
+            'eu-north-1',
             'ap-south-1',
             'ap-southeast-1',
+            'ap-southeast-2',
             'ap-northeast-1',
+            'ap-northeast-2',
+            'ap-east-1',
+            'sa-east-1',
+            'me-south-1',
         ];
+
+        $projectRef = null;
+        if (preg_match('/^db\.([a-z0-9]+)\.supabase\.co$/i', $host, $matches) === 1) {
+            $projectRef = $matches[1];
+        }
+        $userCandidates = [$user];
+        if ($projectRef !== null) {
+            $userCandidates[] = "postgres.{$projectRef}";
+        }
+        $userCandidates = array_values(array_unique($userCandidates));
 
         $lastException = $e;
         foreach ($regions as $region) {
             $poolerHost = "aws-0-{$region}.pooler.supabase.com";
             $poolerDsn = "pgsql:host={$poolerHost};port=6543;dbname={$name};sslmode={$sslmode}";
-            try {
-                $pdo = new PDO($poolerDsn, $user, $pass, $options);
-                break;
-            } catch (PDOException $poolerException) {
-                $lastException = $poolerException;
+            foreach ($userCandidates as $candidateUser) {
+                try {
+                    $pdo = new PDO($poolerDsn, $candidateUser, $pass, $options);
+                    break 2;
+                } catch (PDOException $poolerException) {
+                    $lastException = $poolerException;
+                }
             }
         }
 
